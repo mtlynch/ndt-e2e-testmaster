814d1d659d143a851c382359b450013a14f55a3d
Michael Lynch <mtlynch@google.com>
2016-05-25 16:05:08 -0400

    Adding result processing logic
    The testmaster needs to process output files from
    https://github.com/m-lab/ndt-e2e-clientworker as well as output packages from
    https://github.com/m-lab/ndt-e2e-ansible.
    
    This adds a module to the testmaster that allows it to take a list of files,
    where the files can be result files or a zip of result files, and parse these
    files into NdtResult instances for processing.

---
+++ testmaster/read_results.py
@@ -0,0 +1,122 @@
+# Copyright 2016 Measurement Lab
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import zipfile
+
+from ndt_e2e_clientworker.client_wrapper import result_decoder
+
+
+def parse_files(result_filenames):
+    """Parses a list of files for the NDT results they contain.
+
+    Reads a list of raw files and/or result file packages and parses their
+    contents into NdtResult instances. Any non-result files in the list or
+    within specified packages are ignored.
+
+    Note that if multiple files in the list have the same basename, but
+    different contents results are undefined.
+
+    Args:
+        result_filenames: A list of filenames to parse for results. These may
+            be a combination of raw results (JSON files) and result packages
+            (a compressed archive of raw results).
+
+    Returns:
+        A dictionary of NdtResult instances, keyed by filename (only the
+        basename).
+    """
+    result_files = _read_result_files(result_filenames)
+    return _decode_results(result_files)
+
+
+def _is_raw_result(filename):
+    return os.path.splitext(filename)[1] == '.json'
+
+
+def _is_result_package(filename):
+    return zipfile.is_zipfile(filename)
+
+
+def _read_results_from_package(result_package_filename):
+    """Loads the raw contents of each result file in a result package.
+
+    Opens a result package file and, for each raw result file in the package,
+    reads the raw, decompressed contents of the file into memory. Any filenames
+    in the package that are not result files are ignored.
+
+    Args:
+        result_package_filename: Filename of result package from which to read
+            file contents.
+
+    Returns:
+        A dictionary where each key is a file basename and the value is a string
+        containing the contents of the corresponding file.
+    """
+    results = {}
+    with zipfile.ZipFile(result_package_filename) as result_package:
+        for filename in result_package.namelist():
+            if not _is_raw_result(filename):
+                continue
+            results[os.path.basename(filename)] = result_package.read(filename)
+    return results
+
+
+def _read_result_files(result_filenames):
+    """Loads the raw contents of each result file in result files and packages.
+
+    Given a list of filenames, finds files that looks like either a raw NDT
+    result file or a package of NDT result files. For raw files, we read the
+    file contents into memory directly. For result packages, we open the package
+    and read the contents of each raw result file in the package into memory. We
+    return the results in a dictionary mapping filenames to file contents.
+
+    Args:
+        result_filenames: A list of filenames to parse for results. These may
+            be a combination of raw results (JSON files) and result packages
+            (a compressed archive of raw results).
+
+    Returns:
+        A dictionary where each key is a file basename and the value is a string
+        containing the contents of the corresponding file.
+    """
+    result_files = {}
+    for filename in result_filenames:
+        if _is_raw_result(filename):
+            with open(filename) as result_file:
+                result_files[os.path.basename(filename)] = result_file.read()
+        elif _is_result_package(filename):
+            result_files.update(_read_results_from_package(filename))
+    return result_files
+
+
+def _decode_results(result_files):
+    """Converts a dictionary of raw strings into a dictionary of parsed results.
+
+    Given a dictionary of filenames -> file content strings, parses the a
+    content strings to produce a dictionary of filenames -> NdtResult objects.
+
+    Args:
+        result_files: A dictionary of raw strings to parse into NdtResult
+            objects, keyed by their original filename.
+
+    Returns:
+        A dictionary of NdtResult objects, keyed by the name of the file from
+        which they were parsed.
+    """
+    results = {}
+    decoder = result_decoder.NdtResultDecoder()
+    for filename, raw_contents in result_files.iteritems():
+        results[filename] = decoder.decode(raw_contents)
+    return results
---
+++ tests/test_read_results.py
@@ -0,0 +1,103 @@
+# Copyright 2016 Measurement Lab
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import
+import datetime
+import os
+import unittest
+
+import pytz
+
+from testmaster import read_results
+from testmaster.ndt_e2e_clientworker.client_wrapper import results
+
+# The filename of a raw NDT result file.
+RAW_RESULT_FILENAME = 'raw-result.json'
+# The contents of the raw result file, represented as an NdtResult instance.
+RAW_RESULT = results.NdtResult(
+    browser='chrome',
+    browser_version='50.0.2661.86',
+    end_time=datetime.datetime(2016, 5, 24, 16, 55, 58, 756734, pytz.utc),
+    client='ndt_js',
+    client_version=None,
+    os='OSX',
+    os_version='10.11.3',
+    start_time=datetime.datetime(2016, 5, 24, 16, 55, 22, 677309, pytz.utc),
+    c2s_result=results.NdtSingleTestResult(
+        start_time=datetime.datetime(2016, 5, 24, 16, 55, 34, 74628, pytz.utc),
+        end_time=datetime.datetime(2016, 5, 24, 16, 55, 46, 944071, pytz.utc),
+        throughput=0.938),
+    s2c_result=results.NdtSingleTestResult(
+        start_time=datetime.datetime(2016, 5, 24, 16, 55, 46, 944247, pytz.utc),
+        end_time=datetime.datetime(2016, 5, 24, 16, 55, 58, 324334, pytz.utc),
+        throughput=1.01),
+    latency=564.0)
+# The filename of a package of result files.
+RESULT_PACKAGE_FILENAME = 'result-package.zip'
+# A raw result file within the result package zip.
+PACKAGED_RESULT_FILENAME = 'packaged-result.json'
+# The contents of the packaged result, represented as an NdtResult instance.
+PACKAGED_RESULT = results.NdtResult(
+    browser='chrome',
+    browser_version='50.0.2661.102',
+    end_time=datetime.datetime(2016, 5, 24, 19, 18, 48, 173000, pytz.utc),
+    client='banjo',
+    client_version=None,
+    os='Windows',
+    os_version='2012ServerR2',
+    start_time=datetime.datetime(2016, 5, 24, 19, 18, 3, 924000, pytz.utc),
+    c2s_result=results.NdtSingleTestResult(
+        start_time=datetime.datetime(2016, 5, 24, 19, 18, 35, 219000, pytz.utc),
+        end_time=datetime.datetime(2016, 5, 24, 19, 18, 46, 765000, pytz.utc),
+        throughput=36.1),
+    s2c_result=results.NdtSingleTestResult(
+        start_time=datetime.datetime(2016, 5, 24, 19, 18, 15, 991000, pytz.utc),
+        end_time=datetime.datetime(2016, 5, 24, 19, 18, 25, 715000, pytz.utc),
+        throughput=1.75),
+    latency=79.2)
+# A non-result file that parse_files should ignore.
+GARBAGE_FILENAME = 'garbage.txt'
+
+
+def add_testdata_prefix(filename):
+    """Creates an absolute path to a file in the testdata directory."""
+    testdata_dir = os.path.join(os.path.dirname(__file__), 'testdata')
+    return os.path.join(testdata_dir, filename)
+
+
+class ReadResultsTest(unittest.TestCase):
+
+    def test_reads_raw_result_file_correctly(self):
+        actual_results = read_results.parse_files([add_testdata_prefix(
+            RAW_RESULT_FILENAME)])
+        expected_results = {RAW_RESULT_FILENAME: RAW_RESULT}
+        self.assertDictEqual(expected_results, actual_results)
+
+    def test_reads_packaged_result_file_correctly(self):
+        actual_results = read_results.parse_files([add_testdata_prefix(
+            RESULT_PACKAGE_FILENAME)])
+        expected_results = {PACKAGED_RESULT_FILENAME: PACKAGED_RESULT}
+        self.assertDictEqual(expected_results, actual_results)
+
+    def test_reads_mix_of_input_types_correctly(self):
+        """Parser should process a mix of files and ignore non-result files."""
+        actual_results = read_results.parse_files(
+            [add_testdata_prefix(RAW_RESULT_FILENAME),
+             add_testdata_prefix(RESULT_PACKAGE_FILENAME),
+             add_testdata_prefix(GARBAGE_FILENAME)]) # yapf: disable
+        expected_results = {
+            RAW_RESULT_FILENAME: RAW_RESULT,
+            PACKAGED_RESULT_FILENAME: PACKAGED_RESULT,
+        }
+        self.assertDictEqual(expected_results, actual_results)
