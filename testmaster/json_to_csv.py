d2efc0873258d3d89c2c31ffa8406bfeacf4ebe8
Michael Lynch <mtlynch@google.com>
2016-05-25 14:06:37 -0400

    Temp commit

--- testmaster/json_to_csv.py
+++ testmaster/json_to_csv.py
@@ -4,27 +4,24 @@ import io
 import glob
 import os
 
-from ndt_e2e_clientworker.client_wrapper import result_decoder
-
-
-def _parse_results(result_filenames):
-    results = {}
-    decoder = result_decoder.NdtResultDecoder()
-    for filename in result_filenames:
-        with open(filename) as result_json:
-            results[filename] = decoder.decode(result_json.read())
-    return results
+import read_results
 
 
 def _results_to_csv(results):
     output = io.BytesIO()
-    csv_writer = csv.DictWriter(output, fieldnames=['filename', 'total_duration', 'c2s_throughput', 'c2s_duration', 's2c_throughput', 's2c_duration', 'latency', 'error', 'err_list'])
+    csv_writer = csv.DictWriter(
+        output,
+        fieldnames=['filename', 'total_duration', 'c2s_throughput',
+                    'c2s_duration', 's2c_throughput', 's2c_duration', 'latency',
+                    'error', 'err_list'])
     csv_writer.writeheader()
     for filename, result in results.iteritems():
-        err_list = ','.join([x.message for x in result.errors]) if result.errors else ''
+        err_list = ','.join([x.message for x in result.errors
+                            ]) if result.errors else ''
         row = {
             'filename': os.path.basename(filename),
-            'total_duration': (result.end_time - result.start_time).total_seconds(),
+            'total_duration':
+            (result.end_time - result.start_time).total_seconds(),
             'c2s_throughput': result.c2s_result.throughput,
             's2c_throughput': result.s2c_result.throughput,
             'latency': result.latency,
@@ -32,16 +29,17 @@ def _results_to_csv(results):
             'err_list': err_list,
         }
         if result.c2s_result.end_time and result.c2s_result.start_time:
-            row['c2s_duration'] = (result.c2s_result.end_time - result.c2s_result.start_time).total_seconds()
+            row['c2s_duration'] = (result.c2s_result.end_time -
+                                   result.c2s_result.start_time).total_seconds()
         if result.s2c_result.end_time and result.s2c_result.start_time:
-            row['s2c_duration'] = (result.s2c_result.end_time - result.s2c_result.start_time).total_seconds()
+            row['s2c_duration'] = (result.s2c_result.end_time -
+                                   result.s2c_result.start_time).total_seconds()
         csv_writer.writerow(row)
     return output.getvalue()
 
 
 def main(args):
-    input_files = glob.glob(args.pattern)
-    results = _parse_results(input_files)
+    results = read_results.parse_files(glob.glob(args.pattern))
     print _results_to_csv(results)
 
 
