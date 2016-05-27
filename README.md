7e8d243e8070491ded235e75bcb98eed760d133c
Michael Lynch <mtlynch@google.com>
2016-05-27 11:08:44 -0400

    Adds the JSON to CSV utility. (#4)
    * Adds the JSON to CSV utility.
    
    Adds the CLI front-end to perform JSON to CSV conversion of NDT result files.
    
    Adds a README to describe the utility and provides example usage.

---
+++ README.md
@@ -0,0 +1,26 @@
+# NDT End-to-End TestMaster
+
+[![Build
+Status](https://travis-ci.org/m-lab/ndt-e2e-testmaster.svg?branch=master)](https://travis-ci.org/m-lab/ndt-e2e-testmaster)
+[![Coverage
+Status](https://coveralls.io/repos/m-lab/ndt-e2e-testmaster/badge.svg?branch=master&service=github)](https://coveralls.io/github/m-lab/ndt-e2e-testmaster?branch=master)
+
+## Utilities
+
+### JSON to CSV converter
+
+The JSON to CSV converter takes a collection of raw NDT result JSON files
+(created by the
+[client\_wrapper](https://github.com/m-lab/ndt-e2e-clientworker)) and/or a
+collection of NDT result file packages (created by the [Ansible
+playbook](https://github.com/m-lab/ndt-e2e-ansible)) and gathers the results
+together in a CSV file.
+
+To run the converter:
+
+```bash
+python testmaster/json_to_csv.py --pattern "ndt-results/*" > results.csv
+```
+
+Where `ndt-results` is a folder containing raw JSON results, zips of results, or
+both.
---
+++ testmaster/json_to_csv.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# Copyright 2016 Measurement Lab
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Converts NDT result files to a single CSV describing all results.
+
+Given a pattern of files, finds the NDT result files (JSON) or result packages
+(zips of JSON) and parses their contents to produce a CSV of results.
+"""
+
+import argparse
+import glob
+
+import csv_convert
+import read_results
+
+
+def main(args):
+    results = read_results.parse_files(glob.glob(args.pattern))
+    print csv_convert.ndt_results_to_csv(results)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        prog='NDT Result JSON to CSV converter',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--pattern',
+                        required=True,
+                        help="Glob pattern of input files")
+    main(parser.parse_args())
